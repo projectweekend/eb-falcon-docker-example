712aa9bf5afbd25c5259483eaf462d97bf517572
Brian Hines <brian@projectweekend.net>
2015-07-07 05:50:27 -0500

    New example route

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,4 @@ from hello import handlers as hello_handlers
 
 api.add_route('/', hello_handlers.HelloResource())
 api.add_route('/test', hello_handlers.HelloResource())
+api.add_route('/whatever', hello_handlers.HelloResource())
