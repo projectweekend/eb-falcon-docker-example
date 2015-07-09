ed56b0f882abdc5ee6e1f0eb9a19d33fcde5f353
Brian Hines <brian@projectweekend.net>
2015-07-09 09:50:57 -0500

    Test env variables

--- app/hello/handlers.py
+++ app/hello/handlers.py
@@ -1,3 +1,4 @@
+import os
 import json
 import falcon
 
@@ -9,4 +10,14 @@ class HelloResource(object):
         res.status = falcon.HTTP_200
         res.body = json.dumps({
             'message': "hello!"
+        })
+
+
+class TestVariableResource(object):
+
+    def on_get(self, req, res):
+
+        res.status = falcon.HTTP_200
+        res.body = json.dumps({
+            'message': os.getenv('TEST_THING')
         })
